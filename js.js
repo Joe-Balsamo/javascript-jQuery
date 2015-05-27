b8e394aa65a2ac4c52c8ebfe3729a25d6d19ee45
Cody Hacker <jbalsamo@cox.net>
2015-05-27 09:48:04 -0700

    added random color chooser button

--- js.js
+++ js.js
@@ -7,13 +7,28 @@ $(document).ready(function() {
 
 var $container = $(".wrapper");
 
-var currentColor = "#99CCFF"
+var currentColor = getRandomColor();
+//var currentColor = "#99CCFF";
 
 function doGridButton() {
     $container.html("");
     gridDim = prompt("Please enter grid dimension: ");
     //Now call the grid creator.
     createGrid(gridDim);
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+
+function doColorButton() {
+    currentColor = getRandomColor();
 }
 
 
